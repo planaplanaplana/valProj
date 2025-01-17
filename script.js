3155acf52c796aa58bb8c549e01881eee86a2351
plana <emirplanic1@gmail.com>
2025-01-17 17:01:04 +0100

    test

---
+++ script.js
@@ -0,0 +1,14 @@
+// Example: A simple "Hello" alert (could be changed to any message)
+window.onload = function() {
+    alert("Happy 6 Months Anniversary! Love you so much!");
+  };
+  
+  // If you'd like confetti, you can load a small confetti library from a CDN:
+  // <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+  // Then use something like:
+  // confetti({
+  //   particleCount: 200,
+  //   spread: 70,
+  //   origin: { y: 0.6 }
+  // });
+  
