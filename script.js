012706331810636f6c193554278a265a5f76a126
plana <emirplanic1@gmail.com>
2025-02-13 22:09:19 +0100

    aaaa

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Example: A simple "Hello" alert (could be changed to any message)
 window.onload = function() {
-    alert("Evo ti jedan znak paznje dok ucis volim teeeeee!");
+    alert("I love you");
   };
   
   // If you'd like confetti, you can load a small confetti library from a CDN:
